9a90c99a4c8ada80d601904c1d4082b7090228fb
Wrecless <73550513+Wrecless@users.noreply.github.com>
2023-12-05 15:56:11 +0000

    05/12

---
+++ components/Header.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { twMerge } from 'tailwind-merge';
+import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
+import { HiHome } from 'react-icons/hi';
+
+interface HeaderProps {
+	children: React.ReactNode;
+	className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({
+	children,
+	className,
+}) => {
+	const router = useRouter(); // Corrected variable name
+
+	const handleLogout = () => {
+		//handle logout in the future
+	};
+
+	return (
+		// Corrected placement of return statement
+		<div
+			className={twMerge(
+				`
+                h-fit
+                bg-gradient-to-b
+                from-emerald-800
+                p-6
+            `,
+				className
+			)}
+		>
+			<div
+				className="
+                w-full  // Corrected class name
+                mb-4
+                flex
+                items-center
+                justify-between
+            "
+			>
+				<div
+					className="
+                    hidden
+                    md:flex
+                    gap-x-2
+                    items-center
+                "
+				>
+					<button
+						onClick={() => router.back()} // Corrected method name
+						className="
+                            rounded-full  // Corrected class name
+                            bg-black
+                            flex
+                            items-center
+                            justify-center
+                            hover:opacity-75
+                            transition
+                        "
+					>
+						<RxCaretLeft
+							className="text-white"
+							size={35}
+						/>
+					</button>
+					<button
+						onClick={() => router.forward()} // Corrected method name
+						className="
+                            rounded-full  // Corrected class name
+                            bg-black
+                            flex
+                            items-center
+                            justify-center
+                            hover:opacity-75
+                            transition
+                        "
+					>
+						<RxCaretRight
+							className="text-white"
+							size={35}
+						/>
+					</button>
+				</div>
+				<div className="flex md:hidden gap-x-2 items-center">
+					<HiHome
+						className="text-black"
+						size={20}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
