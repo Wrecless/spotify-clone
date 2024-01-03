e6b7dd084a8145e8d9e74b06018a4255d73eca31
Wrecless <73550513+Wrecless@users.noreply.github.com>
2024-01-03 13:21:16 +0000

    log in added, and register email working properly with supabase

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import AuthModal from '@/components/AuthModal';
+
+import { ProductWithPrice } from '@/types';
+
+interface ModalProviderProps {
+	products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({
+	products,
+}) => {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
+	if (!isMounted) {
+		return null;
+	}
+
+	return (
+		<>
+			<AuthModal />
+		</>
+	);
+};
+
+export default ModalProvider;
